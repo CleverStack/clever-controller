0b227eca11449943b28d6b6d8f3f122323b0de0a
Dmitry Bashkatov <dbashkatov@gmail.com>
2013-07-22 21:47:24 +0400

    add raw express.js perf test

--- test/performance/master.js
+++ test/performance/master.js
@@ -3,12 +3,16 @@ var cp = require('child_process'),
     _ = require('underscore'),
     async = require('async');
 
-function masterMain () {
+function masterMain (module, cb) {
     var clientProcessCount = 5;
     var serverProcessCount = require('os').cpus().length;
 
     _.times(serverProcessCount, function (idx) {
-        cluster.fork();
+        var server = cluster.fork();
+
+        server.send({
+            module: module
+        });
     });
 
     async.times(clientProcessCount, function (idx, cb) {
@@ -24,23 +28,48 @@ function masterMain () {
     }, finish);
 
     function finish (err, results) {
-        if (err) {
-            console.error(err);
-        }
+        cluster.disconnect(function () {
+            if (err) {
+                return cb && cb(err);
+            }
 
-        var result = results.reduce(function (sum, result) {
-            return sum + result;
-        }, 0) / results.length;
+            var result = results.reduce(function (sum, result) {
+                return sum + result;
+            }, 0) / results.length;
 
-        console.log('Average %d requests per second', Math.round(1 / result));
-        cluster.disconnect();
+            cb(null, result);
+        });
     }
 }
 
 if (cluster.isMaster) {
-    console.log('master');
-    masterMain();
+    var jobs = [{
+        name: 'clever-controller',
+        module: './server'
+    }, {
+        name: 'raw express.js',
+        module: './server-express'
+    }];
+
+    // double
+    jobs = jobs.concat(jobs);
+
+    async.forEachSeries(jobs, function (job, cb) {
+        masterMain(job.module, function (err, avg) {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('%s: avg %d requests per second', job.name, Math.round(1 / avg));
+            }
+
+            cb();
+        });
+    }, function (err) {
+    });
+
 }
 else {
-    require('./server');
+    process.on('message', function (msg) {
+        require(msg.module);
+    });
 }
